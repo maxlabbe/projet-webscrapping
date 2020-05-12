a5eca2aac0c25d99ca186378e9c37aae42ee669a
Tessan <51161698+tessanix@users.noreply.github.com>
2020-05-12 21:54:59 +0200

    Add files via upload

--- code_Tessan_et_Max.py
+++ code_Tessan_et_Max.py
@@ -4,10 +4,10 @@ Created on Wed May  6 21:01:04 2020
 
 @author: Tesso
 """
+# In[]
 
 import tkinter as tk
 from tkinter import ttk
-import matplotlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import matplotlib.animation as animation
@@ -16,9 +16,12 @@ from matplotlib import style
 from selenium import webdriver
 from time import sleep
 import data_treatment
-
-matplotlib.use("TkAgg")
+import threading
 style.use('ggplot')
+
+# In[]
+
+
     
 # Principale class of the app that will manage all the frames
 class Marketapp(tk.Tk):
@@ -55,6 +58,8 @@ class Marketapp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
+# In[]
+
 
 # Home page 
 class StartPage(tk.Frame):
@@ -66,17 +71,19 @@ class StartPage(tk.Frame):
         label.pack(pady=10,padx=10)
 
         # Button that open the S&P 500 graph
-        sp500_button = ttk.Button(self, text="S&P 500",
-                            command=lambda: controller.show_frame(SP500))
+        sp500_button = ttk.Button(self, text="S&P 500", command=lambda: controller.show_frame(SP500))
         sp500_button.pack()
         
-        btc_button = ttk.Button(self, text="Bitcoin",
-                            command=lambda: controller.show_frame(BtcApp))
+        btc_button = ttk.Button(self, text="Bitcoin", command=lambda: controller.show_frame(BtcApp))
         btc_button.pack()
+ 
+    
+ # In[]
+ 
  
 # Page with the S&P500 graph
 class SP500(tk.Frame):
-    # Figure wher the graph will be
+    # Figure where the graph will be
     f = Figure(figsize=(7,7), dpi=100)
     gs = f.add_gridspec(2, 1)
     f.subplots_adjust(left = 0.1, bottom = 0.1, right = 0.9, top = 0.9, wspace = 0, hspace = 0.5)
@@ -92,9 +99,10 @@ class SP500(tk.Frame):
     #List of values for 60 seconds
     market_value_60seconds = []
 
-    def __init__(self, parent, controller):
+    def __init__(self,parent, controller):
+       
         tk.Frame.__init__(self, parent)
-
+       
         # Separate the window in 2 frames
         top_frame = tk.Frame(self)
         bottom_frame = tk.Frame(self)
@@ -103,15 +111,21 @@ class SP500(tk.Frame):
         label = tk.Label(top_frame, text="S&P 500")
         label.pack(pady=10,padx=10)
         
+        self.ani = None
         # Button that go to the home page
+        
         home_button = tk.ttk.Button(top_frame, text="Home page", command=lambda: controller.show_frame(StartPage))
         home_button.pack(side=tk.LEFT)
         
-        self.start_pause_flag = False
         
-        # Button to start and pause the graph 
-        start_pause_button = tk.ttk.Button(top_frame, text="Start/Pause", command=lambda: self.start_pause())
-        start_pause_button.pack(side=tk.LEFT)
+        # define if the webdriver has beed launched : initial mode = not launched
+        self.launched = False
+        # define if the animation is running or not
+        self.running = False
+        
+        # Button to start and pause the graph  : will launch the webdriver and start the animation thanks to start_pause function
+        self.start_pause_button = tk.ttk.Button(top_frame, text="Start", command=lambda: self.start_pause())
+        self.start_pause_button.pack(side=tk.LEFT)
         
         # Create a save button
         save_button = tk.ttk.Button(top_frame, text='save', command=lambda: data_treatment.save_to_csv(self.market_values, self.time_values))
@@ -122,8 +136,6 @@ class SP500(tk.Frame):
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         
-        # Anime the graph
-        self.ani = animation.FuncAnimation(self.f, self.animate, interval=1)
         
         #We fill the second graph
         self.year_graph.plot(data_treatment.df_year['Date'], data_treatment.df_year['Adj Close'], color = 'black', label = 'precise values')
@@ -135,19 +147,56 @@ class SP500(tk.Frame):
         top_frame.pack(fill = 'x')
         bottom_frame.pack(fill = 'x')
         
+        
+    """
+    Function to stop or continue graph display
+    param[in] self Instance of SP500: We need to change the start/pause flag 
+    """
+    def start_pause(self):
+        
+        if not self.launched :
+            self.launched = True
+            self.driver_SP500 = webdriver.Firefox()
+            self.driver_SP500.get("https://www.cnbc.com/quotes/?symbol=.SPX")
+            sleep(4)
+        
+            
+        # init the animation :
+        if self.ani is None:      #si l'animation n'est pas lancée alors on retourn self.start()
+            return self.start()
+        
+        
+        if self.running: #si l'animation est en pause alors on la passe en start-mode en appuyant sur le bouton 
+            self.ani.event_source.stop()
+            self.start_pause_button.config(text='Pause')
+        
+        else :
+            self.ani.event_source.start()
+            self.start_pause_button.config(text='Start')
+            
+        self.running = not self.running
+            
+    
+    def start(self):
+        self.start_pause_button.config(text='Pause')
+        self.ani = animation.FuncAnimation(self.f, self.animate, interval= 10)
+        self.ani._start()
+        self.running = True
+        print('started animation')
+            
+
     """
     Fuction that will be use in animation.FuncAnimation to animate the graph
     param[in] self instance of SP500: we will use the graph of the instance
     param[in] interval: frame refresh interval
     """    
     def animate(self, interval):
-        sleep(1)
         
         # Indice of S&P500 at time t
-        market_value = str(data_treatment.driver.find_element_by_xpath("/html/body/div[4]/div/div[3]/div[3]/div[1]/div[2]/div/div[1]/div/table/tbody/tr[5]/td[1]/span[1]").text)
+        market_value = str(self.driver_SP500.find_element_by_xpath("/html/body/div[4]/div/div[3]/div[3]/div[1]/div[2]/div/div[1]/div/table/tbody/tr[5]/td[1]/span[1]").text)
         
         # Data Frame with all the index of the session
-        df_session = data_treatment.create_market_dataFrame(market_value, self.market_values, self.time_values)
+        # df_session = data_treatment.create_market_dataFrame(market_value, self.market_values, self.time_values)
         
         market_value = market_value.replace(',', '')
         self.market_value_60seconds.append(float(market_value))
@@ -155,7 +204,7 @@ class SP500(tk.Frame):
         # We fill the first graph every 60 seconds
         if len(self.market_value_60seconds) == 60:
             df = data_treatment.create_high_low_df(self.market_value_60seconds)
-            colors = {'decrease':'red', 'increase':'green'}
+            colors = {'increase':'green', 'decrease':'red'}
             
             self.real_time_graph.clear()
             self.real_time_graph.bar(df.time_values, df.Diff_High_Low, bottom=df.Low_values, color=df.color)
@@ -167,19 +216,12 @@ class SP500(tk.Frame):
                 self.real_time_graph.bar(df.time_values, df.Diff_High_Low, width=0,color=j,label=i)
             self.real_time_graph.legend()
             self.market_value_60seconds.clear()
-        
-    """
-    Function to stop or continue graph display
-    param[in] self Instance of SP500: We need to change the start/pause flag 
-    """
-    def start_pause(self):
-        if self.start_pause_flag == True:
-            self.ani.event_source.start()
-            self.start_pause_flag = False   
-        
-        else :
-            self.ani.event_source.stop()
-            self.start_pause_flag = True
+
+
+
+
+# In[]
+
 
 class BtcApp(tk.Frame):
     
@@ -190,6 +232,9 @@ class BtcApp(tk.Frame):
 
         button1 = ttk.Button(self, text="Home page", command=lambda: controller.show_frame(StartPage))
         button1.pack()
+      
+        # Set threads:
+        self.btc_thread = threading.Thread(target = self.on_click)
         
         self.running = False
         self.ani = None
@@ -199,7 +244,7 @@ class BtcApp(tk.Frame):
         
         lbl = tk.Label(btns, text="Number of times to run")
         lbl.pack(side=tk.LEFT)
-
+        
         self.points_ent = tk.Entry(btns, width=5)
         self.points_ent.insert(0, '50')
         self.points_ent.pack(side=tk.LEFT)
@@ -211,12 +256,10 @@ class BtcApp(tk.Frame):
         self.interval.insert(0, '2000')
         self.interval.pack(side=tk.LEFT)
         
-        self.driver = webdriver.Firefox()
-        self.driver.get("https://trade.kraken.com/fr-fr/charts/KRAKEN:BTC-USD")
-        sleep(4)
+        self.launched =False
         self.last_values =[]
         
-        self.btn = tk.Button(btns, text='Start', command=self.on_click)
+        self.btn = tk.Button(btns, text='Start', command=self.thread_manager)
         self.btn.pack(side=tk.LEFT)
 
         self.fig = plt.Figure()
@@ -228,10 +271,27 @@ class BtcApp(tk.Frame):
 
         self.ax1.set_ylim(8700,9000)
         self.ax1.set_xlim(0,100)
-
+        
+        
+     def thread_manager(self):
+        # lancer le thread si le driver n'est pas activé
+        if not self.launched:
+            self.btc_thread.start()
+        # si le driver est lancé on close le thread et on rentre direct dans la fonction    
+        else:
+            return self.on_click()
+        
+         
      def on_click(self):
         '''the button is a start, pause and unpause button all in one
         this method sorts out which of those actions to take'''
+
+        if not(self.launched):
+            self.launched = True
+            self.driver = webdriver.Firefox()
+            self.driver.get("https://trade.kraken.com/fr-fr/charts/KRAKEN:BTC-USD")
+            sleep(4)
+        
         if self.ani is None:
             # animation is not running; start it
             return self.start()
@@ -281,7 +341,10 @@ class BtcApp(tk.Frame):
             self.running = False
             self.ani = None
         return self.line,
-       
+    
+    
+# In[]       
+
 def main():
     app = Marketapp()
     app.mainloop()
